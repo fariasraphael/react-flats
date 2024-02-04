8a00691364ba817e368024d689356856f9246993
Raphael Farias <farias.raphael@gmail.com>
2024-02-03 23:54:56 -0300

    add app component

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Flat from './flat.jsx';
+
+class App extends Component {
+	render() {
+		return(
+			<div>
+				<Flat />
+			</div>
+		);
+	}
+}
+
+export default App;
