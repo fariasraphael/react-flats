0dc71393309a4bdc366cca9e59894f1164a15ef4
Raphael Farias <farias.raphael@gmail.com>
2024-02-05 22:50:11 -0300

    Update FlatList and Flat components

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,16 +3,14 @@ import React, { Component } from 'react';
 class Flat extends Component {
 	render() {
 		const style = {
-			//background-image: 
-			backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg')`,
+			backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.props.flat.imageUrl})`,
 		}	
 		return(
 			<div className="flat card" style={style}>
-			  <div className="card-category">150 EUR</div>
+			  <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
 			  <div className="card-description">
-			    <h2>Super 60m2 in trendy neighborhood!</h2>
+			    <h2>{this.props.flat.name}</h2>
 			  </div>
-			  <a className="card-link" href="#"></a>
 			</div>
 		);
 	}
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Flat from "./flat.jsx"
+import flats from "../../data/flats"
 
 class FlatList extends Component {
+
 	render(){
 		return(
-			<div className="col-sm-7">
-				TODO
+			<div className="flat-list col-sm-7">
+				{flats.map((flat) => <Flat flat={flat} key={flat.name} />)}
 			</div>
 		);
 	}
