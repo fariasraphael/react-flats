dfc5f044c1639f8a2401a92cd4e61e764a83c845
Raphael Farias <farias.raphael@gmail.com>
2024-02-07 21:55:18 -0300

    add map component

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React from "react";
+import GoogleMapReact from 'google-map-react';
 
-class Map extends Component {
-	render(){
-		return(
-			<div className="col-sm-5">
-				TODO 32:13
-			</div>
-		);
-	}
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: 48.856614,
+      lng: 2.352222
+    },
+    zoom: 15
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '96vh', width: '50%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDCfSreAa43dnLqzgZUvunpcSmv9rJw-Mk" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+
+      >
+        <AnyReactComponent
+          lat={48.856614}
+          lng={2.352222}
+          text={"HEREEEE 	↓"}
+        />
+      </GoogleMapReact>
+    </div>
+  );
 }
-
-export default Map;
