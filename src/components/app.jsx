653b94aa4f094499b50ef0efca3b381fc6633ab6
Raphael Farias <farias.raphael@gmail.com>
2024-02-12 22:14:30 -0300

    update app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
+	import React, { Component } from 'react';
 
-import FlatList from './flat_list.jsx';
-import Map from './map.jsx'
+	import FlatList from './flat_list.jsx';
+	import Map from './map.jsx'
 
-class App extends Component {
-	render() {
-		return(
-			<div className="row">
-				<FlatList />
-				<Map />
-			</div>
-		);
+	class App extends Component {
+		constructor(props) {
+			super(props);
+
+			this.state = {
+				selectedFlat: "",
+				lat: -22.98423608589928,
+				lng: -43.19922175132725
+			}
+			
+			this.selectFlatApp = this.selectFlatApp.bind(this);
+		}
+
+		selectFlatApp(flat) {
+		    const updatedFlat = flat;
+		    this.setState({
+		        selectedFlat: updatedFlat
+		    }, () => console.log(this.state.selectedFlat.lat));
+		}
+
+
+
+		render() {
+			return(
+				<div className="row">
+					<FlatList selectFlatApp={this.selectFlatApp} selectedFlat={this.state.selectedFlat}/>
+					<Map selectedFlat={this.state.selectedFlat}/>
+				</div>
+			);
+		}
 	}
-}
 
-export default App;
+	export default App;
