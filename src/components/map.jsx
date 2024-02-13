5e857d6362ede6ee57c7953c6369e17c74d2ace4
Raphael Farias <farias.raphael@gmail.com>
2024-02-12 22:13:52 -0300

    add marker and change center after choose aflat

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,30 +1,45 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
+import CustomPin from './custom_pin.jsx'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => (
+  <div style={{ position: 'relative' }}>
+    <CustomPin />
+    <div>{text}</div>
+  </div>
+);
 
-export default function SimpleMap(){
-  const defaultProps = {
-    center: {
+export default function SimpleMap({ selectedFlat }){
+  
+	const center = {
       lat: 48.856614,
       lng: 2.352222
-    },
+    };
+
+  const defaultProps = {
+    center: center,
     zoom: 15
   };
 
+	const lat = selectedFlat ? selectedFlat.lat : defaultProps.center.lat;
+	const lng = selectedFlat ? selectedFlat.lng : defaultProps.center.lng;
+  
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: '96vh', width: '50%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyDCfSreAa43dnLqzgZUvunpcSmv9rJw-Mk" }}
-        defaultCenter={defaultProps.center}
+        center={{
+      lat: lat,
+      lng: lng
+    }}
         defaultZoom={defaultProps.zoom}
 
       >
         <AnyReactComponent
-          lat={48.856614}
-          lng={2.352222}
-          text={"HEREEEE 	↓"}
+          lat={lat} 
+          lng={lng}
+          text={""}
         />
       </GoogleMapReact>
     </div>
